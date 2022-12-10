e68e2f0f98b04c6c1c9953a72560a698f36689f4
Slow1 <skreamlight@gmail.com>
2022-12-10 19:48:37 -0400

    added word and letter counter func

--- main.py
+++ main.py
@@ -5,12 +5,43 @@ path = 'books/frankenstein.txt'
 
 def run():
     text = get_text(path)
-    print(text)
+    words = get_words(text)
+    characters_dictionary = get_letters(text)
+    print("--- Begin report of ", path, ' ---')
+    print(words, ' words were found in the document')
 
+# gets file text
 def get_text(path):
     with open(path) as f:
-     return f.read()
+        return f.read()
    
+# counts words
+def get_words(text):
+    words = text.split()
+    count = len(words)
+    return count
+
+# counts letters 
+def get_letters(text):
+    
+    characters = {
+    }
+
+    for w in text:
+        minus = w.lower()
+
+        if minus in characters:
+            characters[minus] += 1
+
+        else:
+            characters[minus] = 1
+    return(characters)
+    
+    # Turn dict into a list and print string with 
+    # amount of times a letter was typed
+    new_list = list(characters.items())
+    return(new_list)
+  
 
 if __name__ == '__main__':
     run()
